85c1709e7e3104e22214030ca9448703d39f1748
Ted Leary <ted.e.leary@ey.com>
2020-08-26 14:02:22 -0700

    chore(nav): add some icons

--- libs/profile-site-lib/src/lib/nav/src/lib/nav/nav.component.ts
+++ libs/profile-site-lib/src/lib/nav/src/lib/nav/nav.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'profile-site-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NavComponent {
 
 }
