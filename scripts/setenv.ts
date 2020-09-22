b2f83c7d4be7eb56633b41a739e345e52c8516e3
Ted Leary <ted.e.leary@ey.com>
2020-09-22 08:56:56 -0700

    chore(gitignore): adjust git ignore

---
+++ scripts/setenv.ts
@@ -0,0 +1,75 @@
+// run before compilation to create an environment file that contains our .env config
+
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+const { spawn } = require('child_process');
+import * as prompt from 'prompt';
+const colors = require('colors/safe');
+
+prompt.message = colors.white('Enter project name:');
+
+var path = require('path');
+// read environment variables from .env file
+require('dotenv').config();
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+let projectEnvironmentsDirectory;
+
+if (!process.env.OPEN_WEATHER_API_KEY) {
+  console.error('Please provide OPEN_WEATHER_API_KEY in .env file');
+  process.exit(-1);
+}
+
+prompt.start();
+
+prompt.get('projectName', (err, result) => {
+  if (err) {
+    return onError(err);
+  }
+  projectEnvironmentsDirectory = `./apps/${result.projectName}/src/environments/`;
+  const targetPath = isProduction
+    ? `${projectEnvironmentsDirectory}environment.prod.ts`
+    : `${projectEnvironmentsDirectory}environment.ts`;
+  // we have access to our environment variables
+  // in the process.env object thanks to dotenv
+  const environmentFileContent = `
+  export const environment = {
+     production: ${isProduction},
+     OPEN_WEATHER_API_KEY: "${process.env.OPEN_WEATHER_API_KEY}",
+     dataPath: 'assets/data/',
+     stateJSON: 'data.json'
+  };
+  `;
+  // write the content to the respective file
+  writeFile(targetPath, environmentFileContent, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Wrote variables to ${targetPath}`);
+    }
+  });
+
+  const ngServeProject = spawn('ng', ['serve', result.projectName]);
+
+  ngServeProject.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  ngServeProject.stderr.on('data', (data) => {
+    console.log(data);
+  });
+
+  ngServeProject.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+  });
+
+  ngServeProject.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+});
+
+const onError = (err) => {
+  console.log(err);
+  return 1;
+};
